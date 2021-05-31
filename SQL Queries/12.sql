f8d2b6e50d33f945e337c269a282ae8dbdfff84f
Akhmetjyuneid Turan <63200819+Cuneytturan@users.noreply.github.com>
2021-05-31 21:26:15 +0600

    Update 12.sql

--- SQL Queries/12.sql
+++ SQL Queries/12.sql
@@ -2,4 +2,4 @@ INSERT INTO `admin` (`id`, `name`, `email`, `password`, `is_active`) VALUES
 (5, 'Cuneyt', 'cuneytadmin@gmail.com', 'admin', '0')
 
 ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;

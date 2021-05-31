d5dbef7f0ea9f6e8efa459b4f85345f2796ee792
Akhmetjyuneid Turan <63200819+Cuneytturan@users.noreply.github.com>
2021-05-31 22:29:56 +0600

    Create 13.sql

---
+++ SQL Queries/13.sql
@@ -0,0 +1,3 @@
+Select first_name, last_name, email, product_title, qty, product_price
+From user_info join cart on user_info.user_id=cart.user_id join products on cart.p_id=products.product_id
+Order by first_name

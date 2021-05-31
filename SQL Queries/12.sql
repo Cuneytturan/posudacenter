0b15cbd598ac74170fc30e050a8b9bd059a05f50
Akhmetjyuneid Turan <63200819+Cuneytturan@users.noreply.github.com>
2021-05-31 21:26:01 +0600

    Create 12.sql

---
+++ SQL Queries/12.sql
@@ -0,0 +1,5 @@
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `is_active`) VALUES
+(5, 'Cuneyt', 'cuneytadmin@gmail.com', 'admin', '0')
+
+ALTER TABLE `admin`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
